fb123d775bb4ab80f3337f946b8a29c1bf91618f
dogiergin <edogukanergin@gmail.com>
2020-10-28 18:26:02 +0300

    fibonacci created

---
+++ main.py
@@ -0,0 +1,11 @@
+def fibon(m):
+    if ( m == 0 ):
+        return 0
+    elif( m == 1 ):
+        return 1
+    else:
+        return fibon(m-1) + fibon(m-2)
+
+
+g=int(input("istediğiniz fibonacci adımını giriniz : "))
+print(fibon(g))
